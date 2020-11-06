3d029b8bd8942e7063051c4c8082a16c761e9c9d
Dings <deen.job@qq.com>
2020-11-06 22:17:44 +0800

    add bubble sort implement🎈.

--- sort/bubble/bubble.go
+++ sort/bubble/bubble.go
@@ -29,6 +29,26 @@ var (
 			*(r), *(l) = *(l), *(r)
 		}
 	}
+	positiveOrderInt64Func = func(r, l *int64) {
+		if *(r) > *(l) {
+			*(r), *(l) = *(l), *(r)
+		}
+	}
+	reverseOrderInt64Func = func(r, l *int64) {
+		if *(r) < *(l) {
+			*(r), *(l) = *(l), *(r)
+		}
+	}
+	positiveOrderFloat64Func = func(r, l *float64) {
+		if *(r) > *(l) {
+			*(r), *(l) = *(l), *(r)
+		}
+	}
+	reverseOrderFloat64Func = func(r, l *float64) {
+		if *(r) < *(l) {
+			*(r), *(l) = *(l), *(r)
+		}
+	}
 )
 
 // atInt int type sort func
@@ -41,23 +61,19 @@ func atInt(numbers []int, swap func(r, l *int)) {
 }
 
 // atInt64 int64 type sort func
-func atInt64(numbers []float64) {
+func atInt64(numbers []int64, swap func(r, l *int64)) {
 	for i := 0; i < len(numbers); i++ {
 		for j := 0; j < len(numbers)-i-1; j++ {
-			if numbers[j] > numbers[j+1] {
-				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-			}
+			swap(&numbers[j], &numbers[j+1])
 		}
 	}
 }
 
 // atInt Float64 type sort func
-func atFloat64(numbers []float64) {
+func atFloat64(numbers []float64, swap func(r, l *float64)) {
 	for i := 0; i < len(numbers); i++ {
 		for j := 0; j < len(numbers)-i-1; j++ {
-			if numbers[j] > numbers[j+1] {
-				numbers[j], numbers[j+1] = numbers[j+1], numbers[j]
-			}
+			swap(&numbers[j], &numbers[j+1])
 		}
 	}
 }
@@ -75,3 +91,33 @@ func Ints(symbol rune, numbers []int) (err error) {
 	}
 	return
 }
+
+// Int64s is int64 type bubble sort func
+func Int64s(symbol rune, numbers []int64) (err error) {
+	err = nil
+	switch symbol {
+	case '>':
+		atInt64(numbers, positiveOrderInt64Func)
+	case '<':
+		atInt64(numbers, reverseOrderInt64Func)
+	default:
+		err = errors.New("operation symbol error, want > OR < ")
+	}
+	return
+}
+
+// Float64s is float64 type bubble sort func
+func Float64s(symbol rune, numbers []float64) (err error) {
+	err = nil
+	switch symbol {
+	case '>':
+		atFloat64(numbers, positiveOrderFloat64Func)
+	case '<':
+		atFloat64(numbers, reverseOrderFloat64Func)
+	default:
+		err = errors.New("operation symbol error, want > OR < ")
+	}
+	return
+}
+
+// golang type of data => https://studygolang.com/articles/11869
