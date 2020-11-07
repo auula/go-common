944d26a6a4de6da4c2c3b42383635ce102fed798
Dings <deen.job@qq.com>
2020-11-07 21:07:02 +0800

    optimization insertion.go source code.

--- sorting/insertion/insertion.go
+++ sorting/insertion/insertion.go
@@ -3,13 +3,28 @@
 // Author: SDing <deen.job@qq.com>
 // Date: 2020/11/7 - 8:01 下午 - UTC/GMT+08:00
 
+// insertion sorting
 package insertion
 
-var (
-	name = "1"
-)
-
+// Sort is int type sorting func
 func Sort(numbers []int) {
+	for i := 1; i < len(numbers); i++ {
+		pervIndex := i - 1
+		current := numbers[i]
+		// 用上一个元素比较当前的元素
+		for pervIndex >= 0 && numbers[pervIndex] > current {
+			numbers[pervIndex+1] = numbers[pervIndex]
+			pervIndex -= 1
+		}
+		// 如果pervIndex没有变化说明就不需要操作
+		if pervIndex+1 != i {
+			numbers[pervIndex+1] = current
+		}
+	}
+}
+
+// Float64s is float64 type sorting func
+func Float64s(numbers []float64) {
 	for i := 1; i < len(numbers); i++ {
 		pervIndex := i - 1
 		current := numbers[i]
